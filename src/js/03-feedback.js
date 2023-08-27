0489625356c19c02199a54824068c849b62ebbcc
olenaZaruba <z_len4ik@yahoo.fr>
2023-08-27 21:09:33 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import '../css/03-feedback.css';
+import '../css/common.css';
+import throttle from 'lodash.throttle';
+import { refs } from './refs';
 
+const storageForm = 'feedback-form-state';
+
+refs.form.addEventListener('input', throttle(handlerInput, 500));
+refs.form.addEventListener('submit', handlerSubmit);
+
+function handlerInput(evt) {
+  evt.preventDefault();
+  formData = {
+    email: refs.email.value.trim(),
+    message: refs.message.value.trim(),
+  };
+
+  localStorage.setItem(storageForm, JSON.stringify(formData));
+}
+
+function handlerSubmit(evt) {
+  evt.preventDefault();
+  const { email, message } = evt.currentTarget.elements;
+  console.log({ email: email.value.trim(), message: message.value.trim() });
+
+  //   if (localStorage.getItem(storageForm)) {
+  //     let data = JSON.parse(localStorage.getItem(storageForm));
+  //     console.log(data);
+  localStorage.removeItem(storageForm);
+}
+// }
+
+function checkStorageData() {
+  let savedData = localStorage.getItem(storageForm);
+  if (!savedData) {
+    return;
+  }
+  reverseData = JSON.parse(savedData);
+  refs.email.value = reverseData.email ?? '';
+  refs.message.value = reverseData.message ?? '';
+}
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,7 @@
 export const refs = {
   cardsContainer: document.querySelector('.gallery'),
   iframe: document.querySelector('#vimeo-player'),
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
 };
